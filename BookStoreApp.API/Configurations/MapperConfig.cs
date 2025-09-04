5673a184b317bcbfdb36a23affbcc4c30a74af6b
Roger Steve <roxhersbozali@gmail.com>
2025-09-04 19:05:26 -0400

    Setup Book API Controller Setup Book Automapper and Dtos

--- BookStoreApp.API/Configurations/MapperConfig.cs
+++ BookStoreApp.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp.API.Data;
 using BookStoreApp.API.Models.Author;
+using BookStoreApp.API.Models.Book;
 
 namespace BookStoreApp.API.Configurations
 {
@@ -11,8 +12,19 @@ namespace BookStoreApp.API.Configurations
             // Add your mappings here
             // CreateMap<Source, Destination>();
             CreateMap<AuthorCreateDto, Author>().ReverseMap();
+            
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
             CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
+            CreateMap< Book, BookReadOnlyDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+
         }
     }
 }
